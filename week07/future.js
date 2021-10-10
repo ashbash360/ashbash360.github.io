96b85494f5ef5814bf868b57399271fa8ec1ee95
Ashley Sohne <e3607578@student.dcccd.edu>
2021-10-10 15:09:03 -0500

    diff nums

--- week07/future.js
+++ week07/future.js
@@ -3,12 +3,14 @@ var city = ["Dallas", "South Korea", "New York", "California"];
 var spouse = ["Bob", "Mitch", "Carson", "Skylar"];
 var numKids = [0,3,5,1];
 
-var xx = Math.floor(Math.random() * 4)
+var x1 = Math.floor(Math.random() * 4)
+var x2 = Math.floor(Math.random() * 4)
+var x3 = Math.floor(Math.random() * 4)
 
 function tellFortune (job, city, spouse, numKids){
   document.write("You will be a "+ job + " in " + city + ", and married to " + spouse + " with " + numKids + " kids.\n");
 }
 
-tellFortune (job[xx], city[xx], spouse[xx], numKids[xx])
-tellFortune (job[xx], city[xx], spouse[xx], numKids[xx])
-tellFortune (job[xx], city[xx], spouse[xx], numKids[xx])
+tellFortune (job[x1], city[x1], spouse[x1], numKids[x1])
+tellFortune (job[x2], city[x2], spouse[x2], numKids[x2])
+tellFortune (job[x3], city[x3], spouse[x3], numKids[x3])
